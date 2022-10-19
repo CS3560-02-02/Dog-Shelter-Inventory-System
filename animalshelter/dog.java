03a5578cce577f67de06b4ae49bb4935ffc89492
a-zhongg <amyzhong16@gmail.com>
2022-10-18 20:59:24 -0700

    new file:   animalshelter/dog.java

---
+++ animalshelter/dog.java
@@ -0,0 +1,86 @@
+package animalshelter;
+
+public class dog {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private double weight;
+    private String stateOfHealth;
+    private String status;
+    private String breed;
+    private int fee;
+
+public int getId() {
+    return id;
+    }
+
+public void setId(int id) {
+    this.id = id;
+    }
+
+public String getName() {
+    return name;
+    }
+
+public void setName(String name) {
+    this.name = name;
+    }
+
+public int getAge() {
+    return age;
+    }
+
+public void setAge(int age) {
+    this.age = age;
+    }
+
+public String getGender() {
+    return gender;
+    }
+
+public void setGender(String gender) {
+    this.gender = gender;
+    }    
+
+public double getWeight() {
+    return weight;
+    }
+
+public void setWeight(double weight) {
+    this.weight = weight;
+    } 
+    
+public String getStateOfHealth() {
+    return stateOfHealth;
+    }
+
+public void setStateOfHealth(String stateOfHealth) {
+    this.stateOfHealth = stateOfHealth;    
+    } 
+
+public String getstatus() {
+    return status;
+    }
+    
+public void setstatus(String status) {
+    this.status = status;
+    }
+ 
+public String getbreed() {
+    return breed;
+    }
+    
+public void setbreed(String breed) {
+    this.breed = breed;
+    }
+
+public int getFee() {
+    return fee;
+    }
+
+public void setFee(int fee) {
+    this.fee = fee;
+    }        
+
+}
