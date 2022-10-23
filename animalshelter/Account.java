3986e3ad820a6a9f9e11ac34eef1e669a4a37582
ikenton <imkenton@cpp.edu>
2022-10-22 19:37:14 -0700

    Account class and method skeleton

---
+++ animalshelter/Account.java
@@ -0,0 +1,57 @@
+package animalshelter;
+
+public class Account {
+    
+
+    public void createPassword(String password, String id){
+
+    }
+    public void createUsername(String username, String id){
+
+    }
+    public void createName(String firstName, String lastName, int id){
+
+    }
+
+    public void createCustomerID(){
+
+    }
+
+    public void createEmail(String email, String id){
+
+    }
+
+    public void createPhone(int number, String id){
+
+    }
+
+    public void addAddress(String street, String address, String city, String state, String zipcode){
+        //needs to be available for more than 1
+    }
+
+    //getters
+    public String getName(){
+        return "";
+    }
+
+    public String getID(){
+        return "";
+    }
+
+    public String getPassword(){
+        return "";
+    }
+
+    public String getEmail(){
+        return "";
+    }
+
+    public String getPhone(){
+        return "";
+    }
+
+    public String getAddress(){
+        return "";
+    }
+
+}
