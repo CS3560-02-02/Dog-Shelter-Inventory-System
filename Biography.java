18cef8e0462ba642e24eb0e384c69029b9bbc8c1
YvetteCarlos <89424585+YvetteCarlos@users.noreply.github.com>
2022-10-22 17:06:03 -0700

    Add biography file

---
+++ Biography.java
@@ -0,0 +1,43 @@
+package animalshelter;
+
+public class Biography{
+    private String activityLevel;
+    private String temperament;
+    private String dogsLikes;
+    private String dogsDislikes;
+
+    public String getActivityLevel() {
+        return activityLevel;
+    }
+
+    public void setActivityLevel(String activityLevel){
+        this.activityLevel = activityLevel;   
+    }
+
+    public String getTemperament(){
+        return temperament;
+    }
+
+    public void setTemperament(String temperament){
+        this.temperament = temperament;
+    }
+
+    public String getDogsLikes(){
+        return dogsLikes;
+    }
+
+    public void setDogsLikes(String dogsLikes){
+        this.dogsLikes = dogsLikes;
+    }
+
+    public String getDogsDislikes(){
+        return dogsDislikes;
+    }
+
+    public void setDogsDislikes(String dogsDislikes){
+        this.dogsDislikes = dogsDislikes;
+    }
+
+
+
+}
